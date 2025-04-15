c19589df3ef18e65d0501b0d675976026be4804b
aycaRichard12 <aycarichard7@gmail.com>
2025-04-15 04:05:45 -0400

    cambios

--- api/index.js
+++ api/index.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 const cors = require('cors');
-app.use(cors());
-
-app.use(express.json());
-const cochesRouter = require('./routes/coches');
-
-const estadoCocheRouter = require('./routes/estado_vehiculo')
-const marcaCocheRouter = require('./routes/marca')
-const tipoConbustibleCocheRouter = require('./routes/tipo_conbustible')
-const modeloCocheRouter = require('./routes/modelo')
-const tipoCocheRouter = require('./routes/tipo_vehiculo')
-const RegistroCocheRouter = require('./routes/coche')
-const precioCocheRouter = require('./routes/precio')
-const reservasRouter = require('./routes/reservas');
-const busquedaRouter = require('./routes/busqueda')
-app.use('/reservas', reservasRouter);
-app.use('/coches',cochesRouter);
-app.use('/estadocoche',estadoCocheRouter);
-app.use('/marca',marcaCocheRouter);
-app.use('/tipocombustible',tipoConbustibleCocheRouter);
-app.use('/modelo',modeloCocheRouter);
-app.use('/tipo',tipoCocheRouter);
-app.use('/coche',RegistroCocheRouter);
-app.use('/precio',precioCocheRouter);
-app.use('/buscar',busquedaRouter)
-app.get('/', (req, res) => res.send('Hello World!'))
 const path = require('path');
 
-// Servir archivos estáticos desde la carpeta 'public'
+app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// Rutas
+const cochesRouter = require('./routes/coches');
+const estadoCocheRouter = require('./routes/estado_vehiculo');
+const marcaCocheRouter = require('./routes/marca');
+const tipoConbustibleCocheRouter = require('./routes/tipo_conbustible');
+const modeloCocheRouter = require('./routes/modelo');
+const tipoCocheRouter = require('./routes/tipo_vehiculo');
+const RegistroCocheRouter = require('./routes/coche');
+const precioCocheRouter = require('./routes/precio');
+const reservasRouter = require('./routes/reservas');
+const busquedaRouter = require('./routes/busqueda');
+
+// Usar rutas
+app.use('/reservas', reservasRouter);
+app.use('/coches', cochesRouter);
+app.use('/estadocoche', estadoCocheRouter);
+app.use('/marca', marcaCocheRouter);
+app.use('/tipocombustible', tipoConbustibleCocheRouter);
+app.use('/modelo', modeloCocheRouter);
+app.use('/tipo', tipoCocheRouter);
+app.use('/coche', RegistroCocheRouter);
+app.use('/precio', precioCocheRouter);
+app.use('/buscar', busquedaRouter);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
